cef301718bfedcb53de8c7d48647bb841d27affd
kaziba3 <aryanbasnet2521@gmail.com>
2026-04-05 09:08:16 +0545

    bypass google smtp block

--- LenDen/settings.py
+++ LenDen/settings.py
@@ -153,7 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Settings
 # Set to 'django.core.mail.backends.smtp.EmailBackend' for production
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' # Replace with your SMTP host
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
